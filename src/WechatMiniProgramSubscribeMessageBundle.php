0e5c9e150d405f5e59b69c4b8af9a5813baefaf5
github-ci <you@example.com>
2025-11-08 02:20:00 +0800

    fix++++

--- src/WechatMiniProgramSubscribeMessageBundle.php
+++ src/WechatMiniProgramSubscribeMessageBundle.php
@@ -2,7 +2,6 @@
 
 namespace WechatMiniProgramSubscribeMessageBundle;
 
-use BizUserBundle\Entity\BizUser;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle;
 use HttpClientBundle\HttpClientBundle;
@@ -14,25 +13,18 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\DoctrineIndexedBundle\DoctrineIndexedBundle;
 use Tourze\DoctrineResolveTargetEntityBundle\DependencyInjection\Compiler\ResolveTargetEntityPass;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 use Tourze\Symfony\CronJob\CronJobBundle;
 use Tourze\WechatMiniProgramAppIDContracts\MiniProgramInterface;
-use Tourze\WechatMiniProgramUserContracts\UserInterface;
 use WechatMiniProgramAuthBundle\WechatMiniProgramAuthBundle;
 use WechatMiniProgramBundle\Entity\Account;
 use WechatMiniProgramBundle\WechatMiniProgramBundle;
-use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class WechatMiniProgramSubscribeMessageBundle extends Bundle implements BundleDependencyInterface
 {
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
-
-        $container->addCompilerPass(
-            new ResolveTargetEntityPass(UserInterface::class, BizUser::class),
-            PassConfig::TYPE_BEFORE_OPTIMIZATION,
-            1000,
-        );
 
         $container->addCompilerPass(
             new ResolveTargetEntityPass(MiniProgramInterface::class, Account::class),
